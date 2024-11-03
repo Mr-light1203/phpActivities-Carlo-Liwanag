19ec5f27dcd447b431791fdf7cd2f03d42ad7f1b
Mr-light1203 <crlliwanag@gmail.com>
2024-11-03 22:19:32 +0800

    activty 2 successfully created

---
+++ acitvity_2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peys App</title>
+</head>
+<body>
+    <h1>Peys App</h1>
+    
+    <form method="post">
+        <label for="rangeSize">Select Photo Size: </label>
+        <input type="range" name="rangeSize" id="rangeSize" min="10" max="100" value="60"><br> 
+        
+        <label for="slctColorBorder">Select Border Color:</label>
+        <input type="color" name="slctColorBorder" id="slctColorBorder" value="#000000"><br><br> 
+
+        <button type="submit" name="btnProcess">Process</button><br><br>
+    </form>
+
+    <?php 
+    if (isset($_POST['btnProcess'])) { 
+        $borderColor = $_POST['slctColorBorder'];
+        $imgSize = $_POST['rangeSize'];
+        ?>
+        
+        <!-- Display the image with selected border color and size -->
+        <img src="liwanag.jpg" alt="Selected Image" width="<?php echo empty($imgSize) ? '10' : $imgSize; ?>%" 
+             height="<?php echo empty($imgSize) ? '10' : $imgSize; ?>%" 
+             style="border:5px solid <?php echo empty($borderColor) ? '#000000' : $borderColor; ?>;">
+    <?php } ?>
+
+</body>
+</html>
